2d61dce540d1d1d3ce99867c614eed654c30d6d7
Alexander Stoner <alexwstoner2@gmail.com>
2024-05-29 14:15:26 -0600

    Add readme with initial thoughts. Add "final" SQL transformation statement.

---
+++ models/incremental_model.sql
@@ -0,0 +1,19 @@
+MODEL (
+  name splice_case_study.incremental_model,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column month
+  ),
+  start '2023-06-01',
+  cron '@daily',
+  grain (month, user_id)
+);
+
+SELECT
+  month::DATE,
+  user_id::BIGINT,
+  cohort_month::DATE,
+  customer_flag::INT
+FROM
+  splice_case_study.seed_model
+WHERE
+  month BETWEEN @start_date AND @end_date
--- models/seed_model.sql
+++ models/seed_model.sql
@@ -1,14 +1,15 @@
+-- Customer account IDs and subscription status stored monthly.
 MODEL (
   name splice_case_study.seed_model,
   kind SEED (
     path '../seeds/cohort_test_data.csv'
   ),
-  -- No need for additional csv settings (custom delimiters, encoding, etc.) with the sample dataset
+  -- No need for additional csv settings (custom delimiters, encoding, etc.) with the sample dataset.
   columns (
     month DATE,
     user_id BIGINT,
     cohort_month DATE,
     customer_flag INT
   ),
-  grain (id, event_date)
+  grain (month, user_id)
 );
